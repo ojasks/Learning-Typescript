a16970e55049489f656c56bbbd10ee83530fdf77
ojasks <ojaskrsingh24@gmail.com>
2025-08-07 12:47:33 +0530

    better way to write function

--- myFunction.js
+++ myFunction.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
+    // return "hello" //andar-se
 }
+// now it is supposed to return number
 function getUpper(val) {
     return val.toUpperCase();
 }
@@ -10,7 +12,21 @@ function signUpUser(name, email, isPaid) { }
 var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
-addTwo(5);
+//what if u have to return more than one case? 
+var getHello = function (s) {
+    return "";
+};
+var heroes = ["thor", "spiderman", "ironman"];
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
+addTwo(5); //baharse
 getUpper("ojas");
 signUpUser("ojasksksks", "ojasks@gmail.com", true);
 loginUser("oj", "oj@j.com");
