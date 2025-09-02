e96af96aaf0cd78ef622d7766fb0a7009d0abcef
ojasks <ojaskrsingh24@gmail.com>
2025-09-02 23:17:01 +0530

    generics in array and arrow function

--- purets/dist/myGenerics.js
+++ purets/dist/myGenerics.js
@@ -1,4 +1,10 @@
 "use strict";
+// The Problem Generics Solve
+// In TypeScript, when you write functions or classes, you often want them to work with different types, but still keep type safety.
+// If you don’t use generics, you usually fall back to any:
+//This works, but TypeScript won’t catch errors:
+// Enter Generics
+//Generics let you write reusable code that works with any type, while preserving type information.
 Object.defineProperty(exports, "__esModule", { value: true });
 const score = [];
 const names = [];
@@ -23,4 +29,15 @@ function identityFour(val) {
 identityThree("3");
 const result = identityFour({ brand: "ojasss", type: 24 });
 //so yeah generics really solve a great deal and are reusable
+function getSearchProducts(products) {
+    //do some complex calculations
+    const myIndex = 3;
+    return products[myIndex];
+}
+const getMoreSearchProducts = (products) => {
+    //do some database operations
+    const myIndex = 3;
+    return products[myIndex];
+};
+// <T,> this comma is put so that to denote that this sis not a jsx syntax but the generic tag
 //# sourceMappingURL=myGenerics.js.map
--- purets/src/myGenerics.ts
+++ purets/src/myGenerics.ts
@@ -1,3 +1,10 @@
+// The Problem Generics Solve
+// In TypeScript, when you write functions or classes, you often want them to work with different types, but still keep type safety.
+// If you don’t use generics, you usually fall back to any:
+//This works, but TypeScript won’t catch errors:
+// Enter Generics
+//Generics let you write reusable code that works with any type, while preserving type information.
+
 const score: Array<number> = []
 const names: Array<string> = []
 
@@ -35,3 +42,20 @@ interface Bottle{
 
 const result = identityFour<Bottle>({ brand: "ojasss", type: 24 });
 //so yeah generics really solve a great deal and are reusable
+
+function getSearchProducts<T>(products: T[]): T | undefined{
+    //do some complex calculations
+    const myIndex =3
+    return products[myIndex]
+}
+
+
+const getMoreSearchProducts = <T,>(products : T[]): T | undefined=> {
+    //do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+
+// <T,> this comma is put so that to denote that this sis not a jsx syntax but the generic tag
+
+
